09802665b703e1aa3a8137df33e82a4f5998f39c
a-selyugin <selyuginai@gmail.com>
2022-05-03 22:36:56 +0300

    fix: fix singletone issue

--- src/keys.js
+++ src/keys.js
@@ -38,8 +38,7 @@ const rusKeyboardArray = [
 const engKeyboardArray = [
   '~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspase',
   'TAB', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'DEL',
-  // eslint-disable-next-line quotes
-  'CAPS', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', `'`, 'Enter',
+  'CAPS', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter',
   'Shift', '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Up', 'Shift',
   'Ctrl', 'Alt', '', 'Alt', 'Ctrl', 'Left', 'Down', 'Right',
 ];
