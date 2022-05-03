f2344b5e0f9775c4b5abfdb39a0a2771d32ba511
a-selyugin <selyuginai@gmail.com>
2022-05-03 13:28:53 +0300

    feat: add main elements generation

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 import "./assets/styles/style.css";
-import "./style.scss";
+import "./assets/styles/style.scss";
+
+// initialize page
+
+function addElement(tag, className, htmlContent, elementName){
+    let newDiv = document.createElement(tag);
+    newDiv.innerHTML = htmlContent;
+    newDiv.classList.add( className );
+    elementName.append(newDiv);
+}
+
+addElement('div','wrapper','', document.body);
+
+let wrapper = document.querySelector('.wrapper');
+
+addElement('h1','header','Virtual Keyboard for RSSchool', wrapper);
+addElement('textarea', 'main__textarea', 'placeholder', wrapper);
+addElement('div','keyboard__container','', wrapper);
+addElement('p','main__legend','placeholder1',wrapper);
+addElement('p','main__legend','placeholder2',wrapper);
+
+
