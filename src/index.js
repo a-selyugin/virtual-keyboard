1f6edb206a1241e93a08ed9f7908d75dc7d20421
a-selyugin <selyuginai@gmail.com>
2022-05-03 22:34:09 +0300

    refactor: took out appendElementTo function to separate module

--- src/index.js
+++ src/index.js
@@ -2,32 +2,25 @@ import './assets/styles/style.css';
 import './assets/styles/style.scss';
 import keys from './keys';
 import createKeyRow from './createKeyRow';
+import appendElementTo from './appendElementTo'
 
 // initialize page
-
-function addElement(tag, className, htmlContent, elementName) {
-  const newDiv = document.createElement(tag);
-  newDiv.innerHTML = htmlContent;
-  newDiv.classList.add(className);
-  elementName.append(newDiv);
-}
-
 // initialize page wrapper
-addElement('div', 'wrapper', '', document.body);
+appendElementTo('div', 'wrapper', '', document.body);
 const wrapper = document.querySelector('.wrapper');
 
 // create page content
-addElement('h1', 'header', 'Virtual Keyboard for RSSchool', wrapper);
-addElement('textarea', 'main__textarea', 'placeholder', wrapper);
-addElement('div', 'keyboard__container', '', wrapper);
-addElement('p', 'main__legend', 'placeholder1', wrapper);
-addElement('p', 'main__legend', 'placeholder2', wrapper);
+appendElementTo('h1', 'header', 'Virtual Keyboard for RSSchool', wrapper);
+appendElementTo('textarea', 'main__textarea', 'placeholder', wrapper);
+appendElementTo('div', 'keyboard__container', '', wrapper);
+appendElementTo('p', 'main__legend', 'placeholder1', wrapper);
+appendElementTo('p', 'main__legend', 'placeholder2', wrapper);
 
 // create keyboard rows
 const keyboardContainer = document.querySelector('.keyboard__container');
 
 for (let i = 0; i < 5; i += 1) {
-  addElement('div', `keyboard__row-${i + 1}`, '', keyboardContainer);
+  appendElementTo('div', `keyboard__row-${i + 1}`, '', keyboardContainer);
 }
 
 const keyboardRowOne = document.querySelector('.keyboard__row-1');
