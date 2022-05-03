bbcc950da0f758e94297f34448376f93ec663ec2
a-selyugin <selyuginai@gmail.com>
2022-05-03 22:23:51 +0300

    refactor: took out createKeyRow function to separate module

---
+++ src/createKeyRow.js
@@ -0,0 +1,49 @@
+export default function createKeyRow(documentRow, ElementsRow) {
+  for (let i = 0; i < ElementsRow.length; i += 1) {
+    const keyElement = document.createElement('div');
+    keyElement.classList.add('keyboard__key');
+    keyElement.classList.add(ElementsRow[i].name);
+
+    const engSpan = document.createElement('span');
+    engSpan.classList.add('eng');
+
+    const rusSpan = document.createElement('span');
+    rusSpan.classList.add('rus');
+
+    const engProps = Object.entries(ElementsRow[i].eng);
+    const rusProps = Object.entries(ElementsRow[i].rus);
+
+    for (let j = 0; j < engProps.length; j += 1) {
+      const engSpanItem = document.createElement('span');
+      const content = engProps[j][1];
+
+      engSpanItem.classList.add(engProps[j][0]);
+
+      if (engProps[j][0] !== 'lowerCase') {
+        engSpanItem.classList.add('hidden');
+      }
+
+      engSpanItem.innerHTML = content;
+
+      engSpan.append(engSpanItem);
+    }
+
+    for (let j = 0; j < rusProps.length; j += 1) {
+      const rusSpanItem = document.createElement('span');
+      const content = engProps[j][1];
+
+      rusSpanItem.classList.add(engProps[j][0]);
+      rusSpanItem.classList.add('hidden');
+
+      if (content) {
+        rusSpanItem.innerHTML = content;
+      }
+
+      rusSpan.append(rusSpanItem);
+    }
+
+    keyElement.append(engSpan);
+    keyElement.append(rusSpan);
+    documentRow.append(keyElement);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './assets/styles/style.css';
 import './assets/styles/style.scss';
 import keys from './keys';
+import createKeyRow from './createKeyRow';
 
 // initialize page d
 
@@ -34,56 +35,6 @@ const keyboardRowTwo = document.querySelector('.keyboard__row-2');
 const keyboardRowThree = document.querySelector('.keyboard__row-3');
 const keyboardRowFour = document.querySelector('.keyboard__row-4');
 const keyboardRowFive = document.querySelector('.keyboard__row-5');
-
-function createKeyRow(documentRow, ElementsRow) {
-  for (let i = 0; i < ElementsRow.length; i += 1) {
-    const keyElement = document.createElement('div');
-    keyElement.classList.add('keyboard__key');
-    keyElement.classList.add(ElementsRow[i].name);
-
-    const engSpan = document.createElement('span');
-    engSpan.classList.add('eng');
-
-    const rusSpan = document.createElement('span');
-    rusSpan.classList.add('rus');
-
-    const engProps = Object.entries(ElementsRow[i].eng);
-    const rusProps = Object.entries(ElementsRow[i].rus);
-
-    for (let j = 0; j < engProps.length; j += 1) {
-      const engSpanItem = document.createElement('span');
-      const content = engProps[j][1];
-
-      engSpanItem.classList.add(engProps[j][0]);
-
-      if (engProps[j][0] !== 'lowerCase') {
-        engSpanItem.classList.add('hidden');
-      }
-
-      engSpanItem.innerHTML = content;
-
-      engSpan.append(engSpanItem);
-    }
-
-    for (let j = 0; j < rusProps.length; j += 1) {
-      const rusSpanItem = document.createElement('span');
-      const content = engProps[j][1];
-
-      rusSpanItem.classList.add(engProps[j][0]);
-      rusSpanItem.classList.add('hidden');
-
-      if (content) {
-        rusSpanItem.innerHTML = content;
-      }
-
-      rusSpan.append(rusSpanItem);
-    }
-
-    keyElement.append(engSpan);
-    keyElement.append(rusSpan);
-    documentRow.append(keyElement);
-  }
-}
 
 createKeyRow(keyboardRowOne, keys.firstRow);
 createKeyRow(keyboardRowTwo, keys.secondRow);
