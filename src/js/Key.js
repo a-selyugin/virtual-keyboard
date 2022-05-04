7eaf21c758924887ca4eeea0410110865b864b6f
a-selyugin <selyuginai@gmail.com>
2022-05-04 23:16:11 +0300

    feat: separated Key class to own module

---
+++ src/js/Key.js
@@ -0,0 +1,8 @@
+export default class Key {
+  constructor(name, languages, engKey, rusKey) {
+    this.name = name;
+    this.languages = languages;
+    this.engKey = engKey;
+    this.rusKey = rusKey;
+  }
+}
