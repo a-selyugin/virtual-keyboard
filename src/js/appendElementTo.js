fbbe81a53bd54bc28f86b9b0848d032f87eda565
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-05-07 20:34:15 +0300

    Revert "Virtual keyboard"

--- src/js/appendElementTo.js
+++
@@ -1,6 +0,0 @@
-export default function appendElementTo(tag, className, htmlContent, targetElementName) {
-  const newDiv = document.createElement(tag);
-  newDiv.innerHTML = htmlContent;
-  newDiv.classList.add(className);
-  targetElementName.append(newDiv);
-}
